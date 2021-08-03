01d8c25f3608c1a0a6b05b5e5d22cd141dcaf160
lucyjunejack <gkvoid3@gmail.com>
2021-08-02 17:29:59 -0700

    Assignment Completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+
+  var nodeList = [];
+
+  var findNodes = function(node) {
+
+    if (node.classList && node.classList.contains(className)) {
+      nodeList.push(node);
+    }
+
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      findNodes(children[i]);
+    }
+  };
+  findNodes(document.body);
+
+  return nodeList;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = [];
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj;
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+
+  for (var key in obj) {
+    if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+      return '{}';
+    }
+
+    result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+  }
+
+  return '{' + result + '}';
+
 };
